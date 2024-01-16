2b419f563e388baabf82df7ad80b80fab76d4ae2
Miguel <e32cloud@gmail.com>
2024-01-16 01:26:57 +0000

    Created VPC and variables

---
+++ variable.tf
@@ -0,0 +1,126 @@
+# vpc variables
+variable "vpc_cidr" {
+  default       = "10.0.0.0/16"
+  description   = "vpc cidr block"
+  type          = string
+}
+
+variable "public_subnet_az1_cidr" {
+  default       = "10.0.0.0/24"
+  description   = "public subnet az1 cidr block"
+  type          = string
+}
+
+variable "public_subnet_az2_cidr" {
+  default       = "10.0.1.0/24"
+  description   = "public subnet az2 cidr block"
+  type          = string
+}
+
+variable "private_app_subnet_az1_cidr" {
+  default       = "10.0.2.0/24"
+  description   = "private app subnet az1 cidr block"
+  type          = string
+}
+
+variable "private_app_subnet_az2_cidr" {
+  default       = "10.0.3.0/24"
+  description   = "private app subnet az2 cidr block"
+  type          = string
+}
+
+variable "private_data_subnet_az1_cidr" {
+  default       = "10.0.4.0/24"
+  description   = "private data subnet az1 cidr block"
+  type          = string
+}
+
+variable "private_data_subnet_az2_cidr" {
+  default       = "10.0.5.0/24"
+  description   = "private data subnet az2 cidr block"
+  type          = string
+}
+
+# # security group variables
+# variable "ssh_location" {
+#   default       = "0.0.0.0/0"
+#   description   = "the ip address that can ssh into the ec2 instances"
+#   type          = string
+# }
+
+# # rds variables
+# variable "database_snapshot_identifier" {
+#   default       = "arn:aws:rds:us-east-1:776878088985:snapshot:fleetcart-final-snapshot"
+#   description   = "database snapshot arn"
+#   type          = string
+# }
+
+# variable "database_instance_class" {
+#   default       = "db.t2.micro"
+#   description   = "the database instance type"
+#   type          = string
+# }
+
+# variable "database_instance_identifier" {
+#   default       = "dev-rds-db"
+#   description   = "the database instance identifier"
+#   type          = string
+# }
+
+# variable "multi_az_deployment" {
+#   default       = false
+#   description   = "create a standby db instance"
+#   type          = bool
+# }
+
+# # application load balancer variables
+# variable "ssl_certificate_arn" {
+#   default       = "arn:aws:acm:us-east-1:776878088985:certificate/c4b9c5c6-048b-4055-9252-1d15f79abbc3"
+#   description   = "ssl certificate arn"
+#   type          = string
+# }
+
+# # sns topic variables
+# variable "operator_email" {
+#   default       = "eugeniomiguelmendoza@gmail.com"
+#   description   = "a valid email address"
+#   type          = string
+# }
+
+# # auto scaling group variables
+# variable "launch_template_name" {
+#   default       = "dev-launch-template"
+#   description   = "name of the launch template"
+#   type          = string
+# }
+
+# variable "ec2_image_id" {
+#   default       = "ami-0a3836ba4c1c577cf"
+#   description   = "id of the ami"
+#   type          = string
+# }
+
+# variable "ec2_instance_type" {
+#   default       = "t2.micro"
+#   description   = "ec2 instance type"
+#   type          = string
+# }
+
+# variable "ec2_key_pair_name" {
+#   default       = "pemcedarkey"
+#   description   = "name of the ec2 key pair"
+#   type          = string
+# }
+
+# # route 53 variables
+# variable "domain_name" {
+#   default       = "blueastra.co.uk"
+#   description   = "domain name"
+#   type          = string
+# }
+
+# variable "record_name" {
+#   default       = "www"
+#   description   = "sub domain name"
+#   type          = string
+# }
